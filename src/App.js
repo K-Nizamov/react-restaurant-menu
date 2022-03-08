a7a03e9e614a002dfd465e999dfb637080a43ad0
K-Nizamov <hitman91@abv.bg>
2022-03-08 12:09:16 +0200

    Upload the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card';
+
+// , { cache: 'reload' }
 
 function App() {
+  const server = 'https://dev.meta365.eu/wp-json/wp/v2/food?_fields=title,featured_media,content,acf,_links,_embedded&_embed'
+
+  const [type,setType] = useState('All')
+  const [data, setData] = useState([])
+  useEffect(() => {
+
+    fetch(server)
+      .then(res => res.json())
+      .then(result => setData(result))
+      .catch(err => console.log(err))
+
+  }, [])
+
+  console.log(type);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h2 className='header'>Our Menu</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="mealtime-wrapper">
+        <button onClick={()=> setType('All')} className='mealtime-box'>All</button>
+        <button onClick={()=> setType('Breakfast')} className='mealtime-box'>Breakfast</button>
+        <button onClick={()=> setType('Lunch')} className='mealtime-box'>Lunch</button>
+        <button onClick={()=> setType('Shakes')} className='mealtime-box'>Shakes</button>
+        <button onClick={()=> setType('Dinner')} className='mealtime-box'>Dinner</button>
+      </div>
+      <div className='card-wrapper'>
+        {
+          data.map(card => (
+            <Card type={type} key={Math.random()*1000} card={card} />
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+
+function Card({ card, type }) {
+
+    const [display, setDisplay] = useState('flex')
+    const title = card.title.rendered
+    const price = card.acf.price
+    const content = card.content.rendered
+    const category = card._embedded['wp:term'][0][0].name
+    const imageUrl = card._embedded['wp:featuredmedia'][0].source_url
+
+
+    useEffect(() => {
+        if (type === category) {
+            setDisplay('flex')
+        } else if (type === "All") {
+            setDisplay('flex')
+        } else {
+            setDisplay('none')
+        }
+    }, [])
+
+
+    return (
+        <div className="card" style={{ display: display }}>
+            <img className="main-img" src={imageUrl} alt="" />
+            <div className="card-content">
+                <div className="header-wrapper">
+                    <h3 className="header-content">{title}</h3>
+                    <div className="price">${price}</div>
+                </div>
+                <p className="text-content" dangerouslySetInnerHTML={{ __html: content }}></p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
